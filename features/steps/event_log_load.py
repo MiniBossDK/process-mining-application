9bcc3f3a248dc891633a7c39aebc3ea13c147359
Daniel Lerche <s235095@dtu.dk>
2024-11-27 15:32:34 +0100

    Fixed the tests from failing

--- features/steps/event_log_load.py
+++ features/steps/event_log_load.py
@@ -2,17 +2,17 @@ from PySide6.QtCore import Qt, QModelIndex
 from behave import *
 
 import pm4py
-from app.model import EventLogListModel, EventLog
+from app.model import EventLogRepository, EventLog
 
 
 @given('the process miner has successfully opened the application')
 def setup_event_log_model(context):
-    context.event_log_model = EventLogListModel([])
+    context.event_log_model = EventLogRepository()
 
 @when('they select the "Load Event Log" option and choose a valid XES file')
 def user_selected_event_log(context):
     event_log = pm4py.read_xes("features/event_logs/running-example.xes")
-    context.event_log_model.add_event_log(EventLog("running-example.xes", event_log))
+    context.event_log_model.add_event_log(EventLog("running-example.xes", event_log, False))
 
 @then('the application should load the event log data and display it in the list of event logs')
 def verify_loaded_event_log(context):
